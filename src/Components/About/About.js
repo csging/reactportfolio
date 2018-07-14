68dfc8ebf02452d09c82c8e5546c2dc2ebbb6ef5
Christian Gingrich <xian777@gmail.com>
2018-07-14 12:20:42 -0700

    git pages deploy

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,30 +13,21 @@ class About extends Component {
               <div className="col m4 offset-s2 center-align">
                   <img className="circle prof-pic" src={require("../images/me.jpg")} alt=""/>
               </div>
-
-            
-        
-                  <div className="col m3 offset-s1">
+                  <div className="col m6 offset-s1">
                     <br></br><br></br><br></br><br></br>
                     <span className="about-text">
-                        Hi, my name is Christian Gingrich. I'm 25, and almost 2 years
-                        ago I moved to San Diego from Dallas to get a fresh start!
-                        I'm currently learning web dev and I work on projects every day
-                        so I can become the very best, like no one ever was. </span>
+                        Hi, my name is Christian Gingrich. I'm 25, and almost 2 years ago I moved to San Diego from Dallas to get a fresh start!
+                        I'm currently learning web dev and I work on projects every day so I can become the very best, like no one ever was. </span>
                   </div>
                 </div> 
         </div>
         </div>
-        </div>
-       
-         
-          
+        </div> 
 )
     }
 }
 
 export default About;
-
    {/* <div className="section no-pad-bot">
             <div className="name-container">
               <br/><br/>
